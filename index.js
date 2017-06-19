1c9c148835ead94ec77d2c8ffec3ad04d1a3a418
Jan Buschtöns <buschtoens@gmail.com>
2017-06-19 10:16:04 +0200

    set up basic js-joda transpilation

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 /* eslint-env node */
 'use strict';
 
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+
+const jodaPath = path.join(
+  path.dirname(require.resolve('js-joda/package.json')),
+  'src'
+);
+
 module.exports = {
-  name: 'ember-joda'
+  name: 'ember-joda',
+
+  treeForAddon() {
+    const addonTree = this._super.treeForAddon.apply(this, arguments);
+
+    // const babelAddon = this.findAddonByName('ember-cli-babel');
+    const babelAddon = this.addons.find(addon => addon.name === 'ember-cli-babel');
+
+    const jodaTree = new Funnel(jodaPath, {
+      destDir: 'ember-joda'
+    });
+    const transpiledJodaTree = babelAddon.transpileTree(jodaTree);
+
+    return mergeTrees([addonTree, transpiledJodaTree]);
+  }
 };
