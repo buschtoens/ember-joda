b4bb4ca5ab06f6f864c189c232a589d84a0e7437
Jan Buschtöns <buschtoens@gmail.com>
2017-08-17 19:28:14 +0200

    chore(eslint): install prettier and ember-suave

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,19 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  plugins: [
+      'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember-suave/recommended',
+    'prettier'
+  ],
   env: {
     browser: true
   },
   rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'ember-suave/no-const-outside-module-scope': 'off'
   }
 };
