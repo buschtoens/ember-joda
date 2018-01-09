d9514bdb1bd32b9178143e1efdcd08cb261b1f1a
Jan Buschtöns <jan@buschtoens.me>
2018-01-09 10:38:12 +0100

    chore(deps): upgrade everything

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,12 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
-      'prettier'
+    'ember',
+    'prettier'
   ],
   extends: [
     'eslint:recommended',
+    'plugin:ember/recommended',
     'plugin:ember-suave/recommended',
     'prettier'
   ],
@@ -19,5 +21,42 @@ module.exports = {
   rules: {
     'prettier/prettier': ['error', { singleQuote: true }],
     'ember-suave/no-const-outside-module-scope': 'off'
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'index.js',
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'app/**',
+        'addon/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
+    },
+
+    // test files
+    {
+      files: ['tests/**/*.js'],
+      excludedFiles: ['tests/dummy/**/*.js'],
+      env: {
+        embertest: true
+      }
+    }
+  ]
 };
